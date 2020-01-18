4c194ff486c4630db45b68c0497312fdb172687b
shrinidhi99 <shrinidhi99.varna@gmail.com>
2020-01-18 11:42:54 +0530

    intro to nltk

---
+++ nltk-intro.py
@@ -0,0 +1,7 @@
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+example_text = 'Hello there, how are you doing today? I am learning NLTK.'
+
+print(sent_tokenize(example_text))
+
+print(word_tokenize(example_text))
